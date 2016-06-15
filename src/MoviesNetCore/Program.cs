eda16dd654fca31ec4c6acc027f451ea968f71c8
sjmarsh <sjmarsh31@gmail.com>
2016-06-15 14:33:28 +1000

    User port 5000 for docker

--- src/MoviesNetCore/Program.cs
+++ src/MoviesNetCore/Program.cs
@@ -14,6 +14,7 @@ namespace MoviesNetCore
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:5000/")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()

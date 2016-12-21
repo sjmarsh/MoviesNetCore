33d7fb559979e7957d21bf3d891b5aa7ed6d5242
sjmarsh <sjmarsh31@gmail.com>
2016-12-21 14:28:25 +1100

    Added reference data controller

---
+++ src/MoviesNetCore/Controllers/ReferenceDataController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesNetCore.Controllers
+{
+    public class ReferenceDataController : Controller
+    {
+        [HttpGet]
+        [Route("api/referenceData/categories")]
+        public IEnumerable<string> Categories()
+        {
+            var categories = Enum.GetValues(typeof(Category));
+           
+            foreach(var cat in categories)
+            {
+                yield return cat.ToString();
+            }
+        }
+
+    }
+}
